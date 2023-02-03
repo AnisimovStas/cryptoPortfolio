21c2964e05e7202d5a4279fa2b9d211e3bf4baf3
Feralbot <staaasyyaaan@gmail.com>
2023-02-03 11:14:29 +0300

    consolelog price working

--- src/api.js
+++ src/api.js
@@ -14,17 +14,19 @@ socket.addEventListener("message", (e) => {
     PRICE: newPrice,
     PARAMETER: invalidCurrency,
   } = JSON.parse(e.data);
-  if (type == INVALID_SUB) {
-    let preTrimmedInvalidCurrency = invalidCurrency.slice(9);
-    let tailOfPreTrimmedInvalidCurrency =
-      preTrimmedInvalidCurrency.indexOf("~");
-    let trimmedInvalidCurrency = preTrimmedInvalidCurrency.slice(
-      0,
-      tailOfPreTrimmedInvalidCurrency
-    );
-    return console.log("ticker " + trimmedInvalidCurrency + " INVALID");
-  }
+
   if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+    if (type == INVALID_SUB) {
+      let preTrimmedInvalidCurrency = invalidCurrency.slice(9);
+      let tailOfPreTrimmedInvalidCurrency =
+        preTrimmedInvalidCurrency.indexOf("~");
+      let trimmedInvalidCurrency = preTrimmedInvalidCurrency.slice(
+        0,
+        tailOfPreTrimmedInvalidCurrency
+      );
+      invalidCurrencyToBtc(trimmedInvalidCurrency);
+      return console.log("ticker_" + trimmedInvalidCurrency + "_INVALID");
+    }
     return;
   }
 
@@ -72,3 +74,25 @@ export const unsubscribeFromTicker = (ticker) => {
   tickersHandlers.delete(ticker);
   unsubscribeFromTickerOnWs(ticker);
 };
+
+async function invalidCurrencyToBtc(currency) {
+  const response = await fetch(
+    "https://min-api.cryptocompare.com/data/price?fsym=" +
+      currency +
+      "&tsyms=BTC"
+  );
+  const price = await response.json();
+  if (price.BTC) {
+    console.log("ticker " + currency + " price to BTC IS: " + price.BTC);
+    const responseBtcToUsd = await fetch(
+      "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"
+    );
+    const btcPrice = await responseBtcToUsd.json();
+    console.log("BTC price to USD IS: " + btcPrice.USD);
+    const crossBtcPrice = (await price.BTC) / btcPrice.USD;
+    console.log("ticker " + currency + " price to USD IS: " + crossBtcPrice);
+    currency.price = crossBtcPrice;
+  } else {
+    console.log("ticker " + currency + " have'nt trade pair to BTC");
+  }
+}
